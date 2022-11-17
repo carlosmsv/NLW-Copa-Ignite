b8ec09e1095f96d8ca389a623a0319afe5c0884c
Carlos <carlos.msv@hotmail.com>
2022-11-16 21:55:43 -0300

    POST method to create a new pool

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors'
+import { z } from 'zod'
 import { PrismaClient } from '@prisma/client'
+import ShortUniqueId from 'short-unique-id'
 
 const prisma = new PrismaClient({
   log: ['query'],
@@ -18,6 +20,26 @@ async function bootstrap() {
   fastify.get('/pools/count', async () => {
     const count = await prisma.pool.count()
     return {count}
+  })
+  
+  fastify.post('/pools', async (request, reply) => {
+    const createPoolBody = z.object({
+      title: z.string(),
+    })
+
+    const { title } = createPoolBody.parse(request.body)
+
+    const generate = new ShortUniqueId({ length: 6 })
+    const code = String(generate()).toUpperCase()
+
+    await prisma.pool.create({
+      data: {
+         title,
+         code
+      }
+    })
+
+    return reply.status(201).send({ code })
   })
 
   await fastify.listen({port: 3333})
